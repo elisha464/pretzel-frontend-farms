8e4b601cfe390535d48389ac952a0c1da38485b4
PretzelSwap <80453072+PretzelSwap@users.noreply.github.com>
2021-03-13 21:48:38 +0100

    Update types.ts

--- src/state/types.ts
+++ src/state/types.ts
@@ -8,8 +8,8 @@ export interface Farm extends FarmConfig {
   tokenPriceVsQuote?: BigNumber
   poolWeight?: number
   depositFeeBP?: number
-  eggPerBlock?: number
-    userData?: {
+  pretzelPerBlock?: number
+  userData?: {
     allowance: BigNumber
     tokenBalance: BigNumber
     stakedBalance: BigNumber
