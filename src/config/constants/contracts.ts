27b039496095402fac4804662e5d7815028122e2
PretzelSwap <80453072+PretzelSwap@users.noreply.github.com>
2021-03-14 16:32:56 +0100

    Update contracts.ts

--- src/config/constants/contracts.ts
+++ src/config/constants/contracts.ts
@@ -1,30 +1,30 @@
 export default {
-  cake: {
-    56: '0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
-    97: '',
-  },
-  masterChef: {
-    56: '0xe70E9185F5ea7Ba3C5d63705784D8563017f2E57',
-    97: '',
-  },
-  wbnb: {
-    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
-    97: '',
-  },
-  lottery: {
-    56: '',
-    97: '',
-  },
-  lotteryNFT: {
-    56: '',
-    97: '',
-  },
-  mulltiCall: {
-    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
-    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
-  },
-  busd: {
-    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-    97: '',
-  },
-}
+    cake: {
+      56: '0x9066e87Bac891409D690cfEfA41379b34af06391', // to be updated before deployment
+      97: '',
+    },
+    masterChef: {
+      56: '0x36f44a1C8e973739D0034FF1B9B9f6c4c7085625',  // to be updated before deployment
+      97: '0x91F4D7243E82D5059E0038ecB259BfE7e405F896',
+    },
+    wbnb: {
+      56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+      97: '0xae13d989dac2f0debff460ac112a837c89baa7cd',
+    },
+    lottery: {
+      56: '',
+      97: '',
+    },
+    lotteryNFT: {
+      56: '',
+      97: '',
+    },
+    mulltiCall: {
+      56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',  // to be updated before deployment
+      97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+    },
+    busd: {
+      56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+      97: '0x8301f2213c0eed49a7e28ae4c3e91722919b8b47',
+    },
+  }
