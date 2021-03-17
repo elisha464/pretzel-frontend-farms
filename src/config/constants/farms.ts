4b5c2a4c0edc3d8977afcfaec045235fd8f90c29
PretzelSwap <80453072+PretzelSwap@users.noreply.github.com>
2021-03-17 18:17:37 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -1,21 +1,21 @@
 import contracts from './contracts'
 import { FarmConfig, QuoteToken } from './types'
 
-const PRTZL_BUSD_LP_MAINNET = `0x3BB3B98c0FABE30166E6B7D62aEef393249871E4` // to be updated before deployment
-const PRTZL_BNB_LP_MAINNET = `0x5590d45592C846DD820D360dd5Ba6b2610cDeeFB`  // to be updated before deployment
-const PRTZL_MAINNET = `0x9066e87Bac891409D690cfEfA41379b34af06391`  // to be updated before deployment
+const PRTZL_BUSD_LP_MAINNET = `0x8d0e47e3E58aA3a11a236BC7B94E624A2Cca7eb7`
+const PRTZL_BNB_LP_MAINNET = `0xE399899978e32bA3bAdC75fce0B2D391b7ed6302`  
+const PRTZL_MAINNET = `0x4c68ACc056176D1C196f7AA07916450CB734EC5a`  
 
 const farms: FarmConfig[] = [
   {
     risk: 5,
     lpSymbol: 'PRTZL-BUSD LP',
     lpAddresses: {
-      97: '0x11CE35aa3eE3639C30FEF3Af9A359A638B29Fba8',
+      97: '',
       56: PRTZL_BUSD_LP_MAINNET,
     },
     tokenSymbol: 'PRTZL',
     tokenAddresses: {
-      97: '0x294Bc5a393B8de74f8d389197DA639C5FbDB9A07',
+      97: '',
       56: PRTZL_MAINNET,
     },
     quoteTokenSymbol: QuoteToken.BUSD,
@@ -25,12 +25,12 @@ const farms: FarmConfig[] = [
     risk: 5,
     lpSymbol: 'PRTZL-BNB LP',
     lpAddresses: {
-      97: '0x01b1405bEF38652468C9f43861010eB73E0AA443',
+      97: '',
       56: PRTZL_BNB_LP_MAINNET,
     },
     tokenSymbol: 'PRTZL',
     tokenAddresses: {
-      97: '0x294Bc5a393B8de74f8d389197DA639C5FbDB9A07',
+      97: '',
       56: PRTZL_MAINNET,
     },
     quoteTokenSymbol: QuoteToken.BNB,
