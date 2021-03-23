a0c9dd86daf4dd31af77d2170ed1e20c6afba3be
PretzelSwap <80453072+PretzelSwap@users.noreply.github.com>
2021-03-23 17:26:51 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
             <Route path="/farms">
               <Farms />
             </Route>
-            <Route path="/beertent">
+            <Route path="/wiesn">
               <Farms tokenMode />
             </Route>
             {/* <Route path="/pools"> */}
