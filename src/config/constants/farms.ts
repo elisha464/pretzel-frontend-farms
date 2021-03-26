8d77896aae4bec9f971e9ab878e0557897782c8c
PretzelSwap <80453072+PretzelSwap@users.noreply.github.com>
2021-03-26 22:15:10 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -76,12 +76,60 @@ const farms: FarmConfig[] = [
     lpSymbol: 'BUSD',
     lpAddresses: {
       97: '',
-      56: '0x7bc177adeb0a556954d627eff4cb9d3e49f1ad1d', // PRTZL-BUSD LP (BUSD-BUSD will ignore)
+      56: '0x7bc177adeb0a556954d627eff4cb9d3e49f1ad1d', 
     },
     tokenSymbol: 'BUSD',
     tokenAddresses: {
       97: '',
       56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    },
+    quoteTokenSymbol: QuoteToken.BUSD,
+    quoteTokenAdresses: contracts.busd,
+  },
+  {
+    pid: 5,
+    risk: 5,
+    lpSymbol: 'PRTZL-CAKE LP',
+    lpAddresses: {
+      97: '',
+      56: '0x75A0f229eDFa1F4f1eFA46bCfD20337089D72703',
+    },
+    tokenSymbol: 'PRTZL',
+    tokenAddresses: {
+      97: '',
+      56: '0xbb75BB4eBB6cE425D3E11d325720a61bb420d20e',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 6,
+    risk: 4,
+    lpSymbol: 'CAKE-BNB LP',
+    lpAddresses: {
+      97: '',
+      56: '0xa527a61703d82139f8a06bc30097cc9caa2df5a6',
+    },
+    tokenSymbol: 'CAKE',
+    tokenAddresses: {
+      97: '',
+      56: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82',
+    },
+    quoteTokenSymbol: QuoteToken.BNB,
+    quoteTokenAdresses: contracts.wbnb,
+  },
+  {
+    pid: 7,
+    risk: 1,
+    lpSymbol: 'USDT-BUSD LP',
+    lpAddresses: {
+      97: '',
+      56: '0xc15fa3e22c912a276550f3e5fe3b0deb87b55acd',
+    },
+    tokenSymbol: 'USDT',
+    tokenAddresses: {
+      97: '',
+      56: '0x55d398326f99059ff775485246999027b3197955',
     },
     quoteTokenSymbol: QuoteToken.BUSD,
     quoteTokenAdresses: contracts.busd,
